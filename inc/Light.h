21836951f151b8b891631e5396120c5e9e08aadc
Saylor Berman <s.berman@f5.com>
2017-07-12 18:13:23 +0000

    Remove top level directory; clang-format
    Remove top level directory and format all C++ code

--- inc/Light.h
+++ inc/Light.h
@@ -8,19 +8,18 @@
 #ifndef SRC_LIGHT_H_
 #define SRC_LIGHT_H_
 
-#include <vector>
 #include "Model.h"
+#include <vector>
 
 class Light {
 public:
-	Light(float r, float g, float b, float x, float y, float z, bool ambient);
-	virtual ~Light();
+  Light(float r, float g, float b, float x, float y, float z, bool ambient);
+  virtual ~Light();
 
-	float red, green, blue, x, y, z;
-	bool ambient;
+  float red, green, blue, x, y, z;
+  bool ambient;
 
 private:
-
 };
 
 #endif /* SRC_LIGHT_H_ */
